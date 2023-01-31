fc5f03f8817e45b6fd9550ebd499d5f767b35de9
bmigo <berniecmigo@gmail.com>
2023-01-31 14:37:48 -0500

    added js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var apiKey = "07399528a755541d8f71ebec6bd73b42";
-var today = moment().format('L');
+//var today = moment().format('L'); // NOT DEFINED CURRENTLY, CHECK MOMENT
 var searchList = [];
 
+var city;
+
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}";
+var queryURL2 = "http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}";
+
+fetch(queryURL);
+
+$('.searchBtn').on("click", function() {
+    var cityName = document.getElementById("cityInput");
+    console.log(cityName);
+});
+
 
